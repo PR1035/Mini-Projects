9019cf5c4d3b4217f481cd736c8f67cd41931bf5
PR1035 <palakr1035@gmail.com>
2026-06-15 20:26:21 +0530

    started ASCII

--- ASCII ART/main.py
+++ ASCII ART/main.py
@@ -1,1 +1,18 @@
+from PIL import Image
 
+img = Image.open("pineapple.jpg", mode="r")
+
+width, height = img.size
+
+#Getting 2D array of pixels
+rgb_array = []
+for y in range(height):
+    row = []
+    for x in range(width):
+        row.append(img.getpixel((x, y)))
+    rgb_array.append(row)
+print(rgb_array)
+
+
+img.show()
+print(img.size)
