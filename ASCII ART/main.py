2d9bf95ce24cdf752b4c5fa87776ebb2ce8a9d79
PR1035 <palakr1035@gmail.com>
2026-06-16 19:56:48 +0530

    made ascii

--- ASCII ART/main.py
+++ ASCII ART/main.py
@@ -1,4 +1,5 @@
 from PIL import Image
+from colorama import Fore, style
 
 img = Image.open("pineapple.jpg", mode="r")
 
@@ -11,8 +12,33 @@ for y in range(height):
     for x in range(width):
         row.append(img.getpixel((x, y)))
     rgb_array.append(row)
-print(rgb_array)
+# print(rgb_array)
 
+brightness_array = []
+for i in rgb_array:
+    brightness_row = []
+    for j in i:
+        avg = (j[0] + j[1] + j[2])/3.0
+        brightness_row.append(avg)
+    brightness_array.append(brightness_row)
+""" 
+    `^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$
+ """
 
-img.show()
+s = '`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+
+char_array = []
+for i in brightness_array:
+    char_row = []
+    for j in i:
+        variable = int(((j/255.0) * 66) - 1)
+        chara = s[variable]
+        char_row.append(chara)
+    char_array.append(char_row)
+
+for i in char_array:
+    print(i*3)
+
+# img.show()
 print(img.size)
+# print(brightness_array)
